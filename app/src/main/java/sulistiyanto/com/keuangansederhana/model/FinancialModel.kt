74d8799f4ea2aba069b686dbe0214f6c01ec8ddb
sulistiyanto <sulistiyanto404@gmail.com>
2019-01-26 16:02:57 +0700

    have done all

--- app/src/main/java/sulistiyanto/com/keuangansederhana/model/FinancialModel.kt
+++ app/src/main/java/sulistiyanto/com/keuangansederhana/model/FinancialModel.kt
@@ -3,11 +3,13 @@ package sulistiyanto.com.keuangansederhana.model
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
-@Parcelize class FinancialModel(
+@Parcelize
+data class FinancialModel(
     val id: Int?,
     val name: String?,
     val count: String?,
     val description: String?,
     val date: String?,
     val serialNumber: String?,
-    val billNumber: String?) : Parcelable
+    val billNumber: String?
+) : Parcelable
